5183e2653d9793605415b9affd4b764d9f916e8e
skuligowski <sebastian@kuligowski.pl>
2018-05-02 11:52:18 +0200

    Changing place of api.d.ts

--- gulp/declarations/declarations.js
+++ gulp/declarations/declarations.js
@@ -1,11 +1,11 @@
 module.exports = gulp => {
-  gulp.task(`declarations`, cb => {
+  gulp.task(`declarations`, () => {
     const dtsgenerator = require('dtsgenerator');
     const swaggerParser = require('swagger-parser');
     return swaggerParser.bundle('spec/gallery-api.yaml').then(spec => {
       return dtsgenerator.default([spec]).then(api => {
         const fs = require('fs');
-        return fs.writeFile('client/src/app/api.d.ts', api);
+        return fs.writeFile('client/src/api.d.ts', api);
       });
     });
   });
