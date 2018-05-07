35e9bd52889fe9325ba50cf25040c43f7bd736d4
skuligowski <sebastian@kuligowski.pl>
2018-05-08 00:35:11 +0200

    Changing name of the project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ServerlessGallery
+# Image Gallery
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.4.
 
--- client/e2e/app.e2e-spec.ts
+++ client/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('serverless-gallery App', () => {
+describe('image-gallery App', () => {
   let page: AppPage;
 
   beforeEach(() => {
