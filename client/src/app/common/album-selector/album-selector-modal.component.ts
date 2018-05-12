0761e078c01d3d841799e94206202b2185a53fca
skuligowski <sebastian@kuligowski.pl>
2018-05-12 22:53:16 +0200

    RWD for album selector

--- client/src/app/common/album-selector/album-selector-modal.component.ts
+++ client/src/app/common/album-selector/album-selector-modal.component.ts
@@ -1,12 +1,11 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
-import { AlbumSelectorService } from './album-selector.service';
+import { Component, HostBinding } from '@angular/core';
 import { Router } from '@angular/router';
 import { animate, group, query, style, transition, trigger } from '@angular/animations';
 
 const fadeInAnimation = trigger('fadeInAnimation', [
   transition(':enter', group([
     query('.modal', [
-      style({ opacity: 0.7, transform: 'translate3d(40%, 0, 0)' }),
+      style({ opacity: 0.7, transform: 'translate3d(30%, 0, 0)' }),
       animate('.7s 10ms cubic-bezier(.3,.98,.11,1.0)', style({ opacity: 1, transform: 'translate3d(0, 0, 0)'}))
     ]),
     query('.overlay', [
@@ -17,7 +16,7 @@ const fadeInAnimation = trigger('fadeInAnimation', [
   transition(':leave', group([
     query('.modal', [
       style({ opacity: 0.7, transform: 'translate3d(0, 0, 0)' }),
-      animate('.7s 10ms cubic-bezier(.3,.98,.11,1.0)', style({ opacity: 0, transform: 'translate3d(40%, 0, 0)'}))
+      animate('.7s 10ms cubic-bezier(.3,.98,.11,1.0)', style({ opacity: 0, transform: 'translate3d(10%, 0, 0)'}))
     ]),
     query('.overlay', [
       style({ opacity: 0.6 }),
@@ -27,17 +26,17 @@ const fadeInAnimation = trigger('fadeInAnimation', [
 ]);
 
 @Component({
-  selector: 'app-album-selector',
-  templateUrl: './album-selector.component.html',
+  selector: 'app-album-selector-modal',
+  templateUrl: './album-selector-modal.component.html',
   animations: [fadeInAnimation],
   host: { '[@fadeInAnimation]': '' }
 })
-export class AlbumSelectorComponent {
+export class AlbumSelectorModalComponent {
 
   @HostBinding('class.opened')
   opened: boolean;
 
-  constructor(private albumSelectorService: AlbumSelectorService, private router: Router) {
+  constructor(private router: Router) {
 
   }
 
