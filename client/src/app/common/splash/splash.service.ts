d90e9c3bc72371055afb69b573e2dfcc7bfd042b
Sebastian Kuligowski <sebastian.kuligowski@comarch.pl>
2018-07-03 22:56:42 +0200

    Close splash for login page

--- client/src/app/common/splash/splash.service.ts
+++ client/src/app/common/splash/splash.service.ts
@@ -6,24 +6,45 @@ import { filter, first } from 'rxjs/operators';
 export class SplashService {
 
   renderer: Renderer2;
+  splashElement: Element;
 
   constructor(private rendererFactory: RendererFactory2) {
     this.renderer = this.rendererFactory.createRenderer(null, null);
   }
 
-  attachSplash(splashElement: Element): void  {
-    spinnerEvents$
-      .pipe(
-        filter(event => event === SpinnerEvent.STOP),
-        first()
-      ).subscribe(() => this.destroy(splashElement));
+  registerSplash(splashElement: Element): void {
+    this.splashElement = splashElement;
   }
 
-  private destroy(splashElement: Element): void {
-    this.renderer.addClass(splashElement, 'stopped');
-    const unregisterFn = this.renderer.listen(splashElement, 'webkitTransitionEnd', () => {
-      this.renderer.removeChild(splashElement.parentElement, splashElement);
-      unregisterFn();
+  closeOnSpinnerEnd(): void  {
+    if (this.splashElement) {
+      spinnerEvents$
+        .pipe(
+          filter(event => event === SpinnerEvent.STOP),
+          first(),
+        ).subscribe(() => this.closeAndDestroy());
+    }
+  }
+
+  close(): void {
+    this.closeAndDestroy();
+  }
+
+  private closeAndDestroy() {
+    if (this.splashElement) {
+      this.destroy(this.splashElement)
+        .then(() => this.splashElement = null);
+    }
+  }
+
+  private destroy(splashElement: Element): Promise<void> {
+    return new Promise<void>(resolve => {
+      this.renderer.addClass(splashElement, 'stopped');
+      const unregisterFn = this.renderer.listen(splashElement, 'webkitTransitionEnd', () => {
+        this.renderer.removeChild(splashElement.parentElement, splashElement);
+        unregisterFn();
+        resolve();
+      });
     });
   }
 
--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SplashService } from '../common/splash/splash.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private splashService: SplashService) { }
 
   ngOnInit() {
+    this.splashService.close();
   }
 
 }
