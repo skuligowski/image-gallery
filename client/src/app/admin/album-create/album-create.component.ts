9867b98ba48393637186125c1b9dae925da1e62e
skuligowski <sebastian@kuligowski.pl>
2018-08-14 21:54:44 +0200

    Multiple upload poc

--- client/src/app/admin/album-create/album-create.component.ts
+++ client/src/app/admin/album-create/album-create.component.ts
@@ -1,13 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { AlbumsService } from '../../albums.service';
+import { HttpEventType, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-album-create',
   templateUrl: 'album-create.component.html'
 })
 export class AlbumCreateComponent implements OnInit {
-  constructor() {
+  constructor(private albumsService: AlbumsService) {
   }
 
   ngOnInit() {
+  }
+
+  public onDrop(event: DragEvent): void {
+    console.log('drop', event.dataTransfer.files);
+    for (let i = 0; i < event.dataTransfer.files.length; i++) {
+      this.albumsService.upload(event.dataTransfer.files[i]).subscribe(
+        event => {
+          console.log(event);
+          if (event.type === HttpEventType.UploadProgress) {
+            const percentDone = Math.round(100 * event.loaded / event.total);
+            console.log(`File is ${percentDone}% loaded.`);
+          } else if (event instanceof HttpResponse) {
+            console.log('File is completely loaded!');
+          }
+        },
+        (err) => {
+          console.log('Upload Error:', err);
+        }, () => {
+          console.log('Upload done');
+        }
+      );
+    }
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  public onDragOver(event: Event): void {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  public onDragEnter(event: Event): void {
+    event.preventDefault();
+    event.stopPropagation();
   }
 }
