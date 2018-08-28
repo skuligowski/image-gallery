d0458c0f9f4ae55958e57298a155d57bcd149dde
skuligowski <sebastian@kuligowski.pl>
2018-08-28 23:36:52 +0200

    Navigation in admin

--- client/src/app/admin/album-details/album-details.component.ts
+++ client/src/app/admin/album-details/album-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import Image = Definitions.Image;
+import Album = Definitions.Album;
 
 @Component({
   selector: 'app-album-details',
@@ -8,10 +9,12 @@ import Image = Definitions.Image;
 })
 export class AlbumDetailsComponent implements OnInit {
 
+  album: Album;
   images: Image[];
 
   constructor(private route: ActivatedRoute) {
     route.data.subscribe((data: any) => {
+      this.album = data.album;
       this.images = data.album.images;
     });
   }
