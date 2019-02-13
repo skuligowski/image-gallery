f85bdcc3332c0673b30b4f028bfa8913843d738f
Sebastian Kuligowski <sebastian@kuligowski.pl>
2019-02-13 22:33:31 +0100

    Adding library to admin structure

--- client/src/app/admin/admin-routing.module.ts
+++ client/src/app/admin/admin-routing.module.ts
@@ -7,11 +7,13 @@ import { AlbumDetailsResolver } from './album-details.resolver';
 import { LibraryUploadComponent } from './library-manager/library-upload/library-upload.component';
 import { AdminGuard } from '../core/auth/admin.guard';
 import { UsersComponent } from './users/users.component';
+import { LibraryManagerComponent } from './library-manager/library-manager.component';
 
 const adminRoutes: Routes = [
   { path: '', children: [
       { path: 'albums', component: AlbumsManagerComponent, resolve: { albums: AlbumsResolver }, runGuardsAndResolvers: 'always' },
       { path: 'albums/:id', component: AlbumDetailsComponent, resolve: { album: AlbumDetailsResolver, albums: AlbumsResolver }, runGuardsAndResolvers: 'always' },
+      { path: 'library', component: LibraryManagerComponent },
       { path: 'library/upload', component: LibraryUploadComponent },
       { path: 'users', component: UsersComponent },
       { path: '**', redirectTo: 'albums'}
