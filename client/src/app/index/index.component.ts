2057f17784010829241d91e6ece37a6437df42d7
skuligowski <sebastian@kuligowski.pl>
2020-05-03 01:08:32 +0200

    tech: migration of index and login

--- client/src/app/index/index.component.ts
+++ client/src/app/index/index.component.ts
@@ -6,7 +6,8 @@ import { ConfigService } from '../core/config/config.service';
 
 @Component({
   selector: 'app-index',
-  templateUrl: './index.component.html',
+  templateUrl: 'index.component.html',
+  styleUrls: ['index.component.scss']
 })
 export class IndexComponent implements OnInit {
 
--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -6,8 +6,8 @@ import { AuthService } from '../core/auth/auth.service';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.less']
+  templateUrl: 'login.component.html',
+  styleUrls: ['login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
