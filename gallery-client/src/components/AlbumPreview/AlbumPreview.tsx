4d6eb8ed6e9fa64aa50a2db195cc39fdfc571d05
skuligowski <sebastian@kuligowski.pl>
2022-10-23 22:11:58 +0200

    feat: image preview

--- gallery-client/src/components/AlbumPreview/AlbumPreview.tsx
+++ gallery-client/src/components/AlbumPreview/AlbumPreview.tsx
@@ -1,21 +1,20 @@
-import { selectCurrentAlbum } from "../../state/albums/albumSlice";
-import { useAppSelector } from "../../state/hooks";
 import ImagePreview from "./ImagePreview";
 import LazyLoadingGrid from "./LazyLoadingGrid";
 import { useAlbum, useAlbumRoute } from "./useAlbum";
+import style from './AlbumPreview.module.scss';
 
 const AlbumPreview: React.FC = () => {
     useAlbumRoute();
     const { album, image, loading, error } = useAlbum();
     return (
-        <>
+        <div className={style.container}>
             {loading ? <div>Album is loading</div> : null}
             {error ? <div>{error}</div> : null}
             {!loading && album && !error ? (
                 <LazyLoadingGrid album={album} images={album.images} />
             ) : null}
             {image ? <ImagePreview /> : null}
-        </>
+        </div>
     );
 }
 
--- gallery-client/src/components/AlbumPreview/ImagePreview.tsx
+++ gallery-client/src/components/AlbumPreview/ImagePreview.tsx
@@ -1,9 +1,40 @@
 import style from './ImagePreview.module.scss';
 import { useAlbum } from './useAlbum';
+import { useAppDispatch } from '../../state/hooks';
+import { selectImage } from '../../state/albums/albumSlice';
 
 const ImagePreview: React.FC = () => {
-    const { image } = useAlbum();
-    return <div className={style.container}>{image?.filename}</div>
+    const { album, image } = useAlbum();
+    const dispatch = useAppDispatch();
+    const prevImage = () => {
+        const index = album?.images.findIndex(item => item.filename === image?.filename)
+        if (index !== undefined && index > -1 && album?.images[index - 1] !== undefined) {
+            dispatch(selectImage(album.images[index - 1]));
+        }
+    }
+    const nextImage = () => {
+        const index = album?.images.findIndex(item => item.filename === image?.filename);
+        if (index !== undefined && index > -1 && album?.images[index + 1] !== undefined) {
+            dispatch(selectImage(album.images[index + 1]));
+        }
+    }
+    const close = () => {
+        dispatch(selectImage(undefined));
+    }
+    return (
+        <div className={style.container}>
+            <div className={style.imageFrame}>
+                <img src={`/library/${image?.url}`} />
+            </div>
+            <div className={style.navigation}>
+                <div className={style.actions}>
+                    <a className={style.navButton} onClick={close}>X</a>
+                </div>
+                <a className={`${style.navButton} ${style.prev}`} onClick={prevImage}>Left</a>
+                <a className={`${style.navButton} ${style.next}`} onClick={nextImage}>Right</a>
+            </div>
+        </div>
+    );
 }
 
 export default ImagePreview;
