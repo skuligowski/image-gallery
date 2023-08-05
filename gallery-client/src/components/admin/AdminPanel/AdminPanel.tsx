e975af9516f00d63da1f7d8bf864541a5d3e32af
skuligowski <sebastian@kuligowski.pl>
2023-08-05 21:46:33 +0200

    feat: admin panel view

--- gallery-client/src/components/admin/AdminPanel/AdminPanel.tsx
+++ gallery-client/src/components/admin/AdminPanel/AdminPanel.tsx
@@ -1,9 +1,27 @@
-import { useLocation } from "react-router-dom";
-import { environment } from "../../../utils/enviroment";
 import { withAdminAuth } from "../../../hooks/withAdminAuth";
+import { useAppDispatch } from "../../../state/hooks";
+import { useEffect } from "react";
+import { fetchAlbums } from "../../../state/albums/albumsSlice";
+import { fetchConfig } from "../../../state/config/configSlice";
+import SidePanel from "../../client/SidePanel/SidePanel";
+import style from "./AdminPanel.module.scss";
+import NavigationPanel from "../../client/NavigationPanel/NavigationPanel";
 
 const AdminPanel: React.FC = withAdminAuth(() => {
-    return <>Hello Admin!</>
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        dispatch(fetchAlbums());
+        dispatch(fetchConfig());
+    }, []);
+    return (
+        <div className={style.container}>
+            <SidePanel />
+            <div>
+                <NavigationPanel />
+                <div>Hello Admin</div>
+            </div>
+        </div>
+    )
 })
 export default AdminPanel;
   
