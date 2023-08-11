df74e5acb8eb3100399eb6171f525f3af0a8d1d9
skuligowski <sebastian@kuligowski.pl>
2023-08-11 23:27:35 +0200

    feat: albums list in admin

--- gallery-client/src/components/admin/AlbumsList/AlbumsList.tsx
+++ gallery-client/src/components/admin/AlbumsList/AlbumsList.tsx
@@ -1,36 +1,52 @@
-import { Table, Tag } from 'antd';
+import { Tag } from 'antd';
+import { useMemo } from 'react';
 import { selectAlbums } from 'src/state/albums/albumsSlice';
 import { useAppSelector } from 'src/state/hooks';
+import { Album } from 'src/types/api';
+import style from './AlbumsList.module.scss';
+
+const monthMapping: { [key: string]: string } = {
+  '01': 'sty',
+  '02': 'lut',
+  '03': 'mar',
+  '04': 'kwi',
+  '05': 'maj',
+  '06': 'cze',
+  '07': 'lip',
+  '08': 'sie',
+  '09': 'wrz',
+  '10': 'paz',
+  '11': 'lis',
+  '12': 'gru',
+};
+
+const compareFn = (a: Album, b: Album): number => (a.date && b.date ? b.date.localeCompare(a.date) : 0);
 
 const AlbumsList: React.FC = () => {
   const { albums, loading } = useAppSelector(selectAlbums);
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: 'Permalink',
-      dataIndex: 'permalink',
-      key: 'permalink',
-    },
-    {
-      title: 'Status',
-      dataIndex: 'active',
-      key: 'active',
-      render: (active: boolean) => (active ? <Tag color='green'>Active</Tag> : <Tag>Inactive</Tag>),
-    },
-    {
-      title: 'Size',
-      dataIndex: 'size',
-      key: 'size',
-    },
-  ];
+  const albumsSorted = useMemo(() => [...albums].sort(compareFn), [albums]);
+
   return !loading ? (
-    <>
-      <Table dataSource={albums} columns={columns} size="small" pagination={{pageSize: 20}}/>
-    </>
+    <div>
+      {albumsSorted.map((album) => (
+        <div className={style.albumView}>
+          <div className={style.imageWrapper}>
+            <img src={'/library' + album.thumbUrl} />
+          </div>
+          <div className={style.albumData}>
+            <div>
+              <div className={style.name}>{album.name}</div>
+              <div className={style.permalink}>{album.permalink}</div>
+              <div>{album.active ? <Tag color="green">Active</Tag> : <Tag>Inactive</Tag>}</div>
+            </div>
+            <div>
+              <span className={style.year}>&nbsp;{album.date?.substring(0, 4) || ''}</span>
+              <span className={style.month}> | {monthMapping[album.date?.substring(5, 7) || '']}</span>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   ) : null;
 };
 
